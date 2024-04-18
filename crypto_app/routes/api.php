d7dceb075290b0668615234fd342552b3afc003c
kholoud001 <kholoud.sanak@gmail.com>
2024-04-18 16:57:34 +0100

    api routing

--- crypto_app/routes/api.php
+++ crypto_app/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EchangeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::get('/exchanges', [EchangeController::class, 'getExchanges']);
+//detail page
+Route::get('/exchanges/{id}', [EchangeController::class, 'show']);
+
