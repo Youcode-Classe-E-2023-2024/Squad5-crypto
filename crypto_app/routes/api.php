02eb0eed8ea97e38f4392a476c2bbb51a0b1674b
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-04-19 14:53:01 +0100

    Implémenter l'intégration de l'API pour récupérer les données sur les actifs.

--- crypto_app/routes/api.php
+++ crypto_app/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AssetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::namespace('Api')->group(function () {
+    Route::get('/assets', [AssetController::class, 'index']);
 });
